e67e8af698a492bf2933d2a22ffdab7323a32424
Michael Sive <mikesive@gmail.com>
2016-01-05 08:31:15 +1000

    add build-essential

--- installscripts/linux.sh
+++ installscripts/linux.sh
@@ -4,16 +4,16 @@ if [ "$(uname)" = "Darwin" ]; then
     printf "Please install Docker Toolbox, which can be located at:\nhttps://www.docker.com/docker-toolbox\n";
   fi
 elif [ "$(uname)" = "Linux" ]; then
-  echo "Installing ruby dependencies..."
+  printf "Installing ruby dependencies...\n"
   if [ -n "$(command -v yum)" ]; then
-    sudo yum -y install gcc g++ make automake autoconf ruby-devel
+    sudo yum -y install gcc g++ make automake autoconf ruby-devel build-essential
   elif [ -n "$(command -v apt-get)" ]; then
-    sudo apt-get -y install gcc g++ make automake autoconf ruby-devel
+    sudo apt-get -y install gcc g++ make automake autoconf ruby-devel build-essential
   fi
   gem install bundler
   bundle install
   if [ -n "$(command -v docker)" ]; then
-    printf "Docker not detected. Installing Docker..."
+    printf "Docker not detected. Installing Docker...\n"
     curl -sSL https://get.docker.com/ | sh;
     sudo usermod -aG docker ec2-user
   else
