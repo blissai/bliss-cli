5a4f2ccf02fb537b10abdc0b7cfd9475882d07aa
Michael Sive <mikesive@gmail.com>
2016-01-04 14:05:19 +1000

    change echo to printf, string equality to =

--- installscripts/linux.sh
+++ installscripts/linux.sh
@@ -1,12 +1,11 @@
 ##!/bin/bash
 if [ "-n $(command -v docker)" ]; then
-  if [ "$(uname)" == "Darwin" ]; then
-    printf "Please install Docker, which can be located at:\nhttps://www.docker.com/docker-toolbox";
-    printf "Make sure to include Docker Machine.";
-  elif [ "$(uname)" == "Linux" ]; then
-    printf "Installing docker...";
+  if [ "$(uname)" = "Darwin" ]; then
+    printf "Please install Docker, which can be located at:\nhttps://www.docker.com/docker-toolbox\n";
+    printf "Make sure to include Docker Machine.\n";
+  elif [ "$(uname)" = "Linux" ]; then
     curl -sSL https://get.docker.com/ | sh;
     docker pull blissai/collector;
-    printf "Installation complete.";
+    printf "Installation complete.\n";
   fi
 fi
