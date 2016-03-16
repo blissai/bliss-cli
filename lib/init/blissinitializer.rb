3c78fc780a1b86dce54fbb6467383325bdc6935e
Michael Sive <mikesive@gmail.com>
2016-03-16 14:12:38 +1000

    encapsulate logic in projectanalyzer

--- lib/init/blissinitializer.rb
+++ lib/init/blissinitializer.rb
@@ -9,10 +9,10 @@ class BlissInitializer
       puts 'Error: This is not a valid git directory.'.red
       exit
     end
-    @analyzer = ProjectAnalyzer.new(@directory, 500_000)
     load_configuration
     configure_bliss
-    @subdir = prompt_for_subdir
+    @analyzer = ProjectAnalyzer.new(@directory, 500_000)
+    @subdir = @analyzer.prompt_for_subdir
     @args = @subdir.nil? ? [] : ["subdir=#{@subdir}"]
     @docker_runner = DockerInitializer.new(@directory, @config, @args)
     update_repository
@@ -32,29 +32,6 @@ class BlissInitializer
   def execute
     # puts @docker_runner.docker_start_cmd
     @docker_runner.run
-  end
-
-  def prompt_for_subdir(maindir = nil)
-    return nil unless @analyzer.too_big?
-    puts 'This repository appears to consist of multiple projects. ' \
-    'Please choose a subdirectory to analyze (e.g. a node project, a rails project) or type exit.'
-    puts 'Possible choices:'
-    maindir = @directory if maindir.nil?
-    Dir.glob("#{maindir}/*/").each do |sd|
-      puts sd.gsub("#{maindir}/", '')
-    end
-    subdir = $stdin.gets.chomp
-    subdir = subdir.strip
-    exit if subdir == 'exit'
-    full_subdir_path = File.join(maindir, subdir)
-    if File.directory?(full_subdir_path)
-      @analyzer.update_directory(full_subdir_path)
-      return prompt_for_subdir(full_subdir_path) if @analyzer.too_big?
-      return full_subdir_path.gsub("#{@directory}/", '')
-    else
-      puts 'Not a valid subdirectory.'.red
-      prompt_for_subdir(maindir)
-    end
   end
 
   def update_repository
--- lib/utils/projectanalyzer.rb
+++ lib/utils/projectanalyzer.rb
@@ -17,6 +17,23 @@ class ProjectAnalyzer
     @total_lines > @max_lines
   end
 
+  def prompt_for_subdir(maindir = nil, git_dir = nil)
+    return nil unless too_big?
+    maindir = @directory if maindir.nil?
+    git_dir = @directory if git_dir.nil?
+    puts 'This repository appears to consist of multiple projects. ' \
+    'Please choose a subdirectory to analyze (e.g. a node project, a rails project) or type exit.'
+    full_subdir_path = choose_dir(maindir)
+    if File.directory?(full_subdir_path)
+      update_directory(full_subdir_path)
+      return prompt_for_subdir(full_subdir_path, git_dir) if too_big?
+      return full_subdir_path.gsub("#{git_dir}/", '')
+    else
+      puts 'Not a valid subdirectory.'.red
+      prompt_for_subdir(maindir, git_dir)
+    end
+  end
+
   private
 
   def calculate_total_lines
@@ -28,5 +45,16 @@ class ProjectAnalyzer
   def supported_files
     langs = %w(sh rb py go m mm h cpp css js class java php bat ps1 swift f pm pl)
     '-name "' + langs.join('" -o -name "*.') + '"'
+  end
+
+  def choose_dir(maindir)
+    puts 'Possible choices:'
+    Dir.glob("#{maindir}/*/").each do |sd|
+      puts sd.gsub("#{maindir}/", '')
+    end
+    subdir = $stdin.gets.chomp
+    subdir = subdir.strip
+    exit if subdir == 'exit'
+    File.join(maindir, subdir)
   end
 end
