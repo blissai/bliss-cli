f69ff3e61bf91aae18383b6b8758b8d7002dcd02
Michael Sive <mikesive@gmail.com>
2016-03-29 11:57:25 +1000

    add back in args, set in spec

--- lib/init/dockerinitializer.rb
+++ lib/init/dockerinitializer.rb
@@ -3,6 +3,7 @@ class DockerInitializer < DockerRunner
   def initialize(git_dir, env_vars, image_name = 'blissai/collector', pull_latest = true)
     @git_dir = git_dir
     @env_vars = env_vars
+    @args = []
     @image_name = image_name
     @cmd = 'bliss-init'
     pull_image if pull_latest
@@ -14,7 +15,7 @@ class DockerInitializer < DockerRunner
     @env_vars.each do |k, v|
       docker_cmd += " -e \"#{k}=#{v}\""
     end
-    collector_cmds = "ruby /root/collector/bin/#{@cmd}"
+    collector_cmds = "ruby /root/collector/bin/#{@cmd} #{@args.join(' ')}"
     "#{docker_cmd} #{mount_cmd} --rm -t #{@image_name} #{collector_cmds}"
   end
 end
--- spec/docker/dockerintitializer_spec.rb
+++ spec/docker/dockerintitializer_spec.rb
@@ -5,14 +5,17 @@ RSpec.describe DockerInitializer do
       'test_env' => 'testenv',
       'test_env_two' => 'testenvtwo'
     }
+
+    testargs = ['blah=blah', 'blahblah=blahblah']
     @testdir = '/test/mytestrepodir'
     @docker_runner = DockerInitializer.new(@testdir, env_vars, 'test/image', false)
+    @docker_runner.args = testargs
   end
 
   it 'should have the correct command' do
     expected = 'docker run -e "test_env=testenv" -e "test_env_two=testenvtwo"' \
     "  -v #{@testdir}:/repository" \
-    ' --rm -t test/image ruby /root/collector/bin/bliss-init'
+    ' --rm -t test/image ruby /root/collector/bin/bliss-init blah=blah blahblah=blahblah'
     expect(@docker_runner.docker_start_cmd).to eq(expected)
   end
 end
