c94fb9d4062ddb7a0cce33d240bc440729b14e83
Michael Sive <mikesive@gmail.com>
2016-03-29 12:04:47 +1000

    fix project analyzer spec, add docker to circle

--- spec/utils/projectanalyzer_spec.rb
+++ spec/utils/projectanalyzer_spec.rb
@@ -1,17 +1,18 @@
 require_relative '../spec_helper'
 RSpec.describe ProjectAnalyzer do
   before(:all) do
-
+    @testdir = File.expand_path('~/testrepo')
+    `git clone https://github.com/mikesive/Slicki.git #{@testdir}`
   end
 
   it 'should be too big' do
-    @pa = ProjectAnalyzer.new(Dir.pwd, 5)
+    @pa = ProjectAnalyzer.new(@testdir, 3)
     expect(@pa.too_big?).to eq(true)
   end
 
   it 'should not be too big' do
     # TODO Don't hardcode a value, 100m is fine for now
-    @pa = ProjectAnalyzer.new(FileUtils.pwd, 100_000_000)
+    @pa = ProjectAnalyzer.new(@testdir, 100_000_000)
     expect(@pa.too_big?).to eq(false)
   end
 end
