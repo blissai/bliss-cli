2b432e463e7b2f3f28b7b80d5bdd6af394f63ea2
Michael Sive <mikesive@gmail.com>
2016-06-17 12:22:46 +1000

    mock docker call

--- spec/utils/projectanalyzer_spec.rb
+++ spec/utils/projectanalyzer_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe ProjectAnalyzer do
   before(:all) do
     @testdir = File.expand_path('~/testrepo')
     `git clone https://github.com/mikesive/Slicki.git #{@testdir}`
+    allow_any_instance_of(DockerLoc).to receive(:run).and_return(69)
   end
 
   it 'should be too big' do
