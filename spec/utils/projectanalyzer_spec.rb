66d9977cc9c40372df041a0bc165ed3c9364bad0
Michael Sive <mikesive@gmail.com>
2016-06-17 12:27:32 +1000

    change spec

--- spec/utils/projectanalyzer_spec.rb
+++ spec/utils/projectanalyzer_spec.rb
@@ -3,15 +3,16 @@ RSpec.describe ProjectAnalyzer do
   before(:all) do
     @testdir = File.expand_path('~/testrepo')
     `git clone https://github.com/mikesive/Slicki.git #{@testdir}`
-    allow_any_instance_of(DockerLoc).to receive(:run).and_return(69)
   end
 
   it 'should be too big' do
+    allow_any_instance_of(DockerLoc).to receive(:run).and_return(69)
     @pa = ProjectAnalyzer.new(@testdir, 3)
     expect(@pa.too_big?).to eq(true)
   end
 
   it 'should not be too big' do
+    allow_any_instance_of(DockerLoc).to receive(:run).and_return(69)
     @pa = ProjectAnalyzer.new(@testdir, 100_000_000)
     expect(@pa.too_big?).to eq(false)
   end
